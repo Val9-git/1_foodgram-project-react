a84a0205d0d9044df829c06afd36d5a4998b2f44
Valeriy Lozitskiy <v.lozitskiy@yandex.ru>
2023-08-05 23:59:02 +0300

    view.py upd

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,8 +12,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from users.models import Subscription, User
-
-from backend.utils import BaseGetAddRemoveMixin
+from utils import BaseGetAddRemoveMixin
 
 from .serializers import (CustomUserSerializer, IngredientSerializer,
                           RecipeCreateSerializer, RecipeReadSerializer,
